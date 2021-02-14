f41e837b935864c99060a5d42c1c006a639ae59d
jaxzzxaj <programyourlife1020@gmail.com>
2021-02-14 10:24:07 +0900

    ユーザー情報入力にバリデーションを設定
    入力したままを空にしたままアカウントを作成しようとする場合、エラーを起こしてアカウントを作成不可にする実装。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #nameカラムに空のの場合は保存しない
+  validates :name, presence: true
 end
