1fad133763e3c349bad3caa4a4e67378a196aeaa
jaxzzxaj <programyourlife1020@gmail.com>
2021-02-15 11:30:23 +0900

    room&userのアソシエーション
    中間テーブルを設けると多対多をなくす

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   #nameカラムに空のの場合は保存しない
   validates :name, presence: true
+
+  has_many :room_users
+  has_many :rooms, through: :romm_users
 end
