755842633a2c422fb2164397d527cee99f70dc7e
jaxzzxaj <programyourlife1020@gmail.com>
2021-02-19 09:56:31 +0900

    userのテストコード作成
    議題：validates:name,presence=true
    
    １）どの時に使う？（create action)
    2)テストをするためにfactorybot.buildで値を作成
    3)現在テストしているのは何をしているのかを細分化させる。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  descibe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
 
+    it 'nameとemail,passwordとpassword_confirmationが存在すれば登録できること' do
+      expect(@user).to be valid
+    end
+
+    it 'nameが空では登録できないこと' do
+      @user.name=''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name cant be blank")
+    end
+
+    it 'emailが空では登録できないこと' do
+      @user.email=''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email cant be blank")
+    end
+
+    it 'passwordが空では登録できないこと' do
+      @user.password=''
+      @user.valid
+      expected(@user.errors.full_messages).to include("Password cant be blank")
+
+    end
+  end
 end
