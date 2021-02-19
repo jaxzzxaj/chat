95037a3752d1c672b9dc8ee5c1f07fc40b49a346
jaxzzxaj <programyourlife1020@gmail.com>
2021-02-19 10:04:37 +0900

    userテストコードの作成修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,32 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  descibe '#create' do
+  describe '#create' do
     before do
       @user = FactoryBot.build(:user)
     end
 
     it 'nameとemail,passwordとpassword_confirmationが存在すれば登録できること' do
-      expect(@user).to be valid
+      expect(@user).to be_valid
     end
 
     it 'nameが空では登録できないこと' do
-      @user.name=''
+      @user.name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Name cant be blank")
+      expect(@user.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'emailが空では登録できないこと' do
-      @user.email=''
+      @user.email = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Email cant be blank")
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
 
     it 'passwordが空では登録できないこと' do
-      @user.password=''
-      @user.valid
-      expected(@user.errors.full_messages).to include("Password cant be blank")
-
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
   end
 end
