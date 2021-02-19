e18250ef98fe4e78207a9df75d283daad3ae7ac1
jaxzzxaj <programyourlife1020@gmail.com>
2021-02-19 09:16:58 +0900

    投稿するものを指定する
    画像のみ、テキストのみでもOK

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     association :room
 
     after(:build) do |message|
-      message.image.attach(io: File.open('public/images/testimage.png'), filename: 'test_image.png')
+      message.image.attach(io: File.open('public/images/testimage.png'), filename: 'testimage.png')
     end
   end
 end
