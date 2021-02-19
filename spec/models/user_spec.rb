58e977bf48e3b1fe87fc823e35bae1d94ee82811
jaxzzxaj <programyourlife1020@gmail.com>
2021-02-19 10:25:46 +0900

    userのテスト（重複したメール）
    ・どういったときに登録される？
    create内→saveをするとき。
    ・another　もうひとり
    ・error文は次に登録をしたヒトに対してだから、エラー分userではなくanotherの方へ。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -48,5 +48,12 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
+
+    it '重複したemailが存在している場合登録できないこと' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
   end
 end
